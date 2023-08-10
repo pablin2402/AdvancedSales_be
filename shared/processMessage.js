e60ed92080741a079d7a5a5f646810d62c666674
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2023-08-10 19:23:09 -0400

    mensaje

--- shared/processMessage.js
+++ shared/processMessage.js
@@ -12,7 +12,8 @@ async function Process(textUser, number){
 
     try {
         const dbData = await getListOfTextProcess("CL-01");
-
+        myConsole.log(dbData)
+        console.log(dbData)
         dbData.forEach(doc => {
             processDocument(doc, textUser, number, models);
         });
@@ -32,11 +33,11 @@ async function Process(textUser, number){
 function processDocument(doc, textUser, number, models) {
     const inputMessages = doc.inputMessage;
     const targetMessage = doc.targetMessage;
-
+    myConsole.log(inputMessages, targetMessage)
+    console.log(inputMessages, targetMessage)
     if (inputMessages.some(keyword => textUser.includes(keyword))) {
         var model = whatsappModel.MessageText(targetMessage, number);
         models.push(model);
-
         if (doc.messageType === "image") {
             var modelImage = whatsappModel.MessageImage(doc.link, number);
             models.push(modelImage);
