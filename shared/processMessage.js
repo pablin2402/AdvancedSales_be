19ff0eb1a096f9331b9cc48bbd8b0fbd0c342c8f
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2023-08-17 18:13:29 -0400

    mensaje

--- shared/processMessage.js
+++ shared/processMessage.js
@@ -29,7 +29,7 @@ async function Process(textUser, number){
     }
 }
 async function processDocument(doc, textUser, number, models, dbData, inputMessages, targetMessages, parentChild) {
-    const targetMessage='';
+    let targetMessage='';
     if(parentChild){
         targetMessage = targetMessages
     }
