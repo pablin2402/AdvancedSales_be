df8b426a1a25516f557712041b33950b6c7e053e
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2023-08-19 02:37:48 -0400

    Chatgpt implementation

--- my-synonyms-library/data.js
+++ my-synonyms-library/data.js
@@ -1,5 +1,9 @@
 module.exports = {
     "casa": ["hogar", "vivienda", "residencia"],
     "perro": ["canino", "mascota", "perrito"],
-    "hola":["holi","hi","holo","como estas","hello"]
+    "hola":["holi","hi","holo","como estas","hello"],
+    "hi":["holi","hi","holo","como estas","hello"],
+    "holo":["holi","hi","holo","como estas","hello"],
+    "hello":["holi","hi","holo","como estas","hello"],
+
 };
