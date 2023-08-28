fa3521fdc91afa98e0c0c26837b39263777ac5c5
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2023-08-27 22:30:49 -0400

    mensaje

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -8,7 +8,7 @@ function SendMessageWhatsApp1(data){
         body: data,
         headers: {
             "Content-Type": "application/json",
-            Authorization: "Bearer EAAMaY0K534YBO5rZAi9DmZCcpPDvyoJpDxc8uLvwMxX1qZAjYmyiErZApvNnA8qeZBTZBY4ZATfnyBeKjwhu1GnKqRC2ZBWZCisGlBZACGNzO9ImFiGyuI2923QHdrTwnHbl9rniWXGw8I1xZAySx6cRLf78Y5pP20dTdSS8CAJE1mxbV0CZABPzFduswZCMm8aDJGYflvZCmmPrZCKpO8vMZB5rZAVYZD"
+            Authorization: "Bearer EAAMaY0K534YBO1V0zO9Q6gT2S398GltLEjy4i1ytge5xRRbJKXWFLin4eXsJBt8q2yboZBEHOxwJX5RKz99RKwMiUXWEWXXJNI0ZC3mkUHes7nbRUYyXB8dpzAzsSLm4FmBoyz8pSUHKhXI33FPur6a37gOMlgKlSVx101jb8pVqn9C3rdbTRyP42gYJZA8qyKWlZCmhkPGRtyvHCvcZD"
         }
     };
     const req = https.request(options, res => {
