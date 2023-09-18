0f5550fb04df957b273d8aed38f7d17c221bb0b9
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2023-09-18 01:18:36 -0400

    mensaje

--- shared/processMessage.js
+++ shared/processMessage.js
@@ -72,12 +72,12 @@ async function processDocument(doc, textUser, number, models, dbData, inputMessa
       }
     }
     doc.children.forEach(childDoc => {
+        console.log(childDoc)
         const childDocument = dbData.find(item => item._id.toString() === childDoc.id_parent.toString());
-        console.log(childDocument)
         if (childDocument && !childDoc.processed) {
             childDoc.processed = true; 
             const childInputMessages = childDoc.inputMessage.map(keyword => keyword.toLowerCase());
-            //console.log(childInputMessages.targetMessage)
+            console.log(childDoc.targetMessage)
             const childTargetMessage = childDoc.targetMessage;
             processDocument(childDocument, textUser, number, models, dbData, childInputMessages, childTargetMessage);
         }
