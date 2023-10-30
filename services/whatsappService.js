d0142bfe5c06f153a983dc76f5f2168c5ef2b6ce
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2023-10-30 14:36:52 -0400

    Added code

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -6,7 +6,7 @@ const getClientInfo = async (number) => {
 };
 
 async function SendMessageWhatsApp1(data, number){
-  console.log(number)
+   console.log(number)
     const token = await getClientInfo(number);
     let dataTemplate;
       token.forEach(doc => {
